137bf331350ec04883f01495fcf8bef56989a1dd
Marcio Vasconcelos <matiusbv@gmail.com>
2023-11-29 21:34:48 -0200

    teste2

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,6 +3,6 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("primeiro programa em Java");
+		System.out.println("primeiro programa em Java2");
 	}
 }
